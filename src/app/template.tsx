45ae4a78c58deaf9d98d895f10af68ca6a6209b1
Sandun2002 <sandungunathilaka2002@gmail.com>
2026-05-03 08:48:31 +0530

    bug fix

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -12,7 +12,7 @@ export default function Template({ children }: { children: React.ReactNode }) {
     gsap.fromTo(
       ".page-transition",
       { opacity: 0, y: 20 },
-      { opacity: 1, y: 0, duration: 0.5, ease: "power2.out" }
+      { opacity: 1, y: 0, duration: 0.5, ease: "power2.out", clearProps: "transform" }
     );
   }, [pathname]);
 
